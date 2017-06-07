73a4d8d040739b00f9d268135117280d127b691d
Doug Carroll <dcarroll@esri.com>
2017-06-07 12:18:52 -0700

    Updates for JH

--- src/app.js
+++ src/app.js
@@ -5,11 +5,23 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-app.use(express.static('public'));
-app.get('/', function(req, res) {
+app.use(express.static('public')); // Make the public folder your web root, for hosting files.
+
+app.get('/', function(req, res) { // Express will serve out the index.html file (via GET)
     res.sendfile('./public/index.html');
 });
 
-app.listen(5000, function(){
-    console.log("http server running on port 5000");
+app.post('/weather', function(req, res) {  // This is the weather handler.  It recieves POSTs @ http://localhost:3000/weather.
+    var fooParam = req.body.fooParam;
+    var barParam = req.body.barParam;
+    log.readConfig().then((configuration)=>{ // Inside the weather handler, do something async...
+        var config = JSON.parse(configuration);
+        res.setHeader('Content-Type', 'application/json');
+        var message = JSON.stringify({"success":true, "msg": "Guest time to live (TTL) in sandbox changed.", "subscriptionTtl":expiration});
+        res.send(message); 
+    });
+});
+
+app.listen(3000, function(){
+    console.log("http server running on port 3000");
 });
